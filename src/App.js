3d5a7c6fc3ebe48729c1800cd61bd0a5896a14be
drony0610 <51791640+drony0610@users.noreply.github.com>
2023-02-17 15:23:22 +0300

    message

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route, Outlet, HashRouter } from "react-router-d
 function App() {
   return (
     <div className="MainApp">
-      <HashRouter>
+      <BrowserRouter>
       <Menu/>
         <Routes>
           <Route path="/" element={<Home/>}/>
@@ -15,7 +15,7 @@ function App() {
           <Route path="/history" element={<Outlet/>}/>
           <Route path="/settings" element={<Outlet/>}/>
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
